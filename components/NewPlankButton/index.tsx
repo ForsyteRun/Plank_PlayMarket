bbf954007fb5221dbf725321692b526cb82a15d7
forsyteru <forsyterun@gmail.com>
2025-09-29 12:44:27 +0200

    feat: NewPlankButton

--- components/NewPlankButton/index.tsx
+++ components/NewPlankButton/index.tsx
@@ -1,26 +1,21 @@
-import { useExercises } from "@/context/ExerciseContext";
 import { INIT_PLANK } from "@/data/defaultPlank";
-import { useRouter } from "expo-router";
+import { useManageUserPlankFomList } from "@/hooks";
 import { Pressable, Text } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function NewPlankButton() {
   const { bottom, right } = useSafeAreaInsets();
-  const router = useRouter();
+  const { handlePlankPress } = useManageUserPlankFomList();
 
-  const { setLocalExercises } = useExercises();
-
-  const handleNewPlankScreen = () => {
-    setLocalExercises(INIT_PLANK);
-
-    router.push("/newPlankScreen");
+  const handlePlankScreen = () => {
+    handlePlankPress(INIT_PLANK);
   };
 
   return (
     <Pressable
-      onPress={handleNewPlankScreen}
+      onPressIn={handlePlankScreen}
       style={{ bottom: bottom + 30, right: right + 20 }}
-      className="absolute bottom-[px] right-8 h-16 w-16 items-center justify-center rounded-2xl bg-ORANGE elevation-md"
+      className="absolute bottom right-8 h-16 w-16 items-center justify-center rounded-2xl bg-ORANGE elevation-md"
     >
       <Text className="text-3xl text-white">+</Text>
     </Pressable>
