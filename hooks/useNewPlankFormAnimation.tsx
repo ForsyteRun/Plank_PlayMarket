f45ba539f9d8be5242880b93fdc8abc65fa8d5d1
forsyteru <forsyterun@gmail.com>
2025-10-03 09:54:27 +0200

    fix: worklet useNewPlankFormAnimation

--- hooks/useNewPlankFormAnimation.tsx
+++ hooks/useNewPlankFormAnimation.tsx
@@ -1,36 +1,60 @@
 import { Dimensions, Keyboard } from "react-native";
 import {
+  Easing,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
 
 const containerPadding = 20;
+
 export const useNewPlankFormAnimation = () => {
   const width = Dimensions.get("window").width;
-
   const startWidth = width - containerPadding * 2;
 
   const inputWidth = useSharedValue(startWidth);
   const borderWidth = useSharedValue(1);
   const borderColor = useSharedValue("#65a198");
 
-  const animatedStyle = useAnimatedStyle(() => ({
-    width: inputWidth.value,
-    borderWidth: borderWidth.value,
-    borderColor: borderColor.value,
-  }));
+  const animatedStyle = useAnimatedStyle(() => {
+    "worklet";
+    return {
+      width: inputWidth.value,
+      borderWidth: borderWidth.value,
+      borderColor: borderColor.value,
+    };
+  });
 
   const handleFocus = () => {
-    inputWidth.value = withTiming(startWidth - 40, { duration: 300 });
-    borderWidth.value = withTiming(borderWidth.value + 1, { duration: 300 });
-    borderColor.value = withTiming("#000", { duration: 300 });
+    "worklet";
+    inputWidth.value = withTiming(startWidth - 40, {
+      duration: 300,
+      easing: Easing.inOut(Easing.ease),
+    });
+    borderWidth.value = withTiming(borderWidth.value + 1, {
+      duration: 300,
+      easing: Easing.inOut(Easing.ease),
+    });
+    borderColor.value = withTiming("#000", {
+      duration: 300,
+      easing: Easing.inOut(Easing.ease),
+    });
   };
 
   const handleBlur = () => {
-    inputWidth.value = withTiming(startWidth, { duration: 300 });
-    borderWidth.value = withTiming(borderWidth.value - 1, { duration: 300 });
-    borderColor.value = withTiming("#65a198", { duration: 300 });
+    "worklet";
+    inputWidth.value = withTiming(startWidth, {
+      duration: 300,
+      easing: Easing.inOut(Easing.ease),
+    });
+    borderWidth.value = withTiming(borderWidth.value - 1, {
+      duration: 300,
+      easing: Easing.inOut(Easing.ease),
+    });
+    borderColor.value = withTiming("#65a198", {
+      duration: 300,
+      easing: Easing.inOut(Easing.ease),
+    });
 
     Keyboard.dismiss();
   };
